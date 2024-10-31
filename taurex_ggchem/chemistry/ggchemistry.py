07a849ed97ef30d9241196d3f1a59aef5e48879b
Ahmed <ahmed.al-refaie.12@ucl.ac.uk>
2024-10-31 12:30:06 +0000

    Updating unsafe GGchem

--- taurex_ggchem/chemistry/ggchemistry.py
+++ taurex_ggchem/chemistry/ggchemistry.py
@@ -9,9 +9,12 @@ import numpy.typing as npt
 
 # from taurex.util.fortran import FortranSafeCaller
 import importlib.resources as ires
-from taurex.util.fortran import stdout_redirector
+from taurex.util.fortran import SafeFortranCaller, FortranStopException
+from taurex.exceptions import InvalidModelException
 
-selected = [
+PathType = t.Union[str, pathlib.Path, os.PathLike]
+
+selected: t.List[str] = [
     "H",
     "He",
     "C",
@@ -39,23 +42,18 @@ selected = [
 ]
 
 
-class StreamToLogger:
-
-    def __init__(self, logger):
-        self.logger = logger
-
-    def write(self, buf):
-        for line in buf.rstrip().splitlines():
-            out = line.rstrip()
-
-            if isinstance(out, bytes):
-                out = out.decode("utf-8")
-            self.logger.debug(buf)
-
-
 class GGChemNonSafe(Chemistry):
+    """GGChem chemistry class.
 
-    allowed_elements = [
+    This implements GGChem through spawning a Fortran process and calling the GGChem Fortran code.
+    The reason for this is the ``STOP`` command that GGChem uses to halt the code when an error occurs
+    which essentially kills the Python process. We use a FortranSafeCaller which spawns a process to
+    catch this error and reinitialize the GGChem code. Its not perfect but stops the code from crashing.
+
+
+    """
+
+    allowed_elements: t.List[str] = [
         "H",
         "He",
         "Li",
@@ -99,32 +97,71 @@ class GGChemNonSafe(Chemistry):
         "W",
     ]
 
-    element_index_dict = dict(
+    element_index_dict: t.Dict[str, int] = dict(
         zip(allowed_elements[1:], range(0, len(allowed_elements)))
     )
 
     def __init__(
         self,
-        dispol_files=None,
-        abundance_profile=None,
-        selected_elements=selected,
-        ratio_elements=None,
-        ratios_to_O=None,
-        he_h_ratio=0.083,
-        metallicity=1.0,
-        include_charge=False,
-        equilibrium_condensation=False,
-        dustchem_file=None,
-        Tfast=1000,
-        new_back_it=6,
-        new_back_fac=1e5,
-        new_pre_method=2,
-        new_full_it=False,
-        new_fast_level=1,
-    ):
+        dispol_files: t.Optional[t.List[PathType]] = None,
+        abundance_profile: t.Optional[str] = None,
+        selected_elements: t.Optional[t.List[str]] = selected,
+        ratio_elements: t.Optional[t.List[str]] = None,
+        ratios_to_O: t.Optional[npt.ArrayLike] = None,
+        he_h_ratio: t.Optional[float] = 0.083,
+        metallicity: t.Optional[float] = 1.0,
+        include_charge: t.Optional[bool] = False,
+        equilibrium_condensation: t.Optional[bool] = False,
+        dustchem_file: t.Optional[PathType] = None,
+        Tfast: t.Optional[float] = 1000,
+        new_back_it: t.Optional[int] = 6,
+        new_back_fac: t.Optional[float] = 1e5,
+        new_pre_method: t.Optional[int] = 2,
+        new_full_it: t.Optional[bool] = False,
+        new_fast_level: t.Optional[int] = 1,
+    ) -> None:
         super().__init__(self.__class__.__name__)
 
-        self._stream_to_logger = StreamToLogger(self)
+        self._init_dispol_files = dispol_files
+        self._init_abundance_profile = abundance_profile
+        self._init_selected_elements = selected_elements
+        self._init_ratio_elements = ratio_elements
+        self._init_ratios_to_O = ratios_to_O
+        self._init_he_h_ratio = he_h_ratio
+        self._init_metallicity = metallicity
+        self._init_include_charge = include_charge
+        self._init_equilibrium_condensation = equilibrium_condensation
+        self._init_dustchem_file = dustchem_file
+        self._init_Tfast = Tfast
+        self._init_new_back_it = new_back_it
+        self._init_new_back_fac = new_back_fac
+        self._init_new_pre_method = new_pre_method
+        self._init_new_full_it = new_full_it
+        self._init_new_fast_level = new_fast_level
+
+        self.reinitialize_ggchem()
+        self.restore_molecule_names()
+        self._setup_active_inactive()
+        self.add_ratio_params()
+
+    def reinitialize_ggchem(self) -> None:
+        """Initialize GGchem for the first time or after a STOP error."""
+        dispol_files = self._init_dispol_files
+        abundance_profile = self._init_abundance_profile
+        selected_elements = self._init_selected_elements
+        ratio_elements = self._init_ratio_elements
+        ratios_to_O = self._init_ratios_to_O
+        he_h_ratio = self._init_he_h_ratio
+        metallicity = self._init_metallicity
+        include_charge = self._init_include_charge
+        equilibrium_condensation = self._init_equilibrium_condensation
+        dustchem_file = self._init_dustchem_file
+        Tfast = self._init_Tfast
+        new_back_it = self._init_new_back_it
+        new_back_fac = self._init_new_back_fac
+        new_pre_method = self._init_new_pre_method
+        new_full_it = self._init_new_full_it
+        new_fast_level = self._init_new_fast_level
 
         elements = [s.strip() for s in selected_elements]
         if "O" not in elements:
@@ -134,14 +171,9 @@ class GGChemNonSafe(Chemistry):
         if "H" not in elements:
             elements.insert(0, "H")
 
-        if include_charge:
-            elements.append("el")
-
         self._he_h_ratio = he_h_ratio
         self._metallicity = metallicity
         self._base_data_path = ires.files("taurex_ggchem") / "data"
-
-        self._charge = "el" in elements
 
         self.info("Using GGChem chemistry")
 
@@ -152,11 +184,13 @@ class GGChemNonSafe(Chemistry):
         if len(ignored_elements) > 0:
             self.info("Elements ignored: %s", ignored_elements)
 
-        if self._charge:
-            self.info("Ions activated")
-
         elements = [s.strip() for s in elements if s in self.allowed_elements]
-
+        if include_charge:
+            self._charge = include_charge
+            elements.append("el")
+            self.info("Ions activated")
+        else:
+            self._charge = False
         fchem.chemistry.newfullit = new_full_it
         fchem.chemistry.newbackit = new_back_it
         fchem.chemistry.newbackfac = new_back_fac
@@ -179,21 +213,27 @@ class GGChemNonSafe(Chemistry):
                     "dispol_WoitkeRefit.dat",
                 ]
             ]
+        else:
+            dispol_files = [pathlib.Path(s) for s in dispol_files]
+            for dispol in dispol_files:
+                if not dispol.exists():
+                    raise FileNotFoundError(f"Dispol file {dispol} not found")
 
-        fchem.parameters.elements = " ".join([s.ljust(2) for s in elements]).ljust(200)
+            fchem.parameters.elements = " ".join([s.ljust(2) for s in elements]).ljust(
+                200
+            )
+
         # print('ELEMENTS',fchem.parameters.elements)
         self.info("Elements in system: %s", elements)
         dispol = [str(s).ljust(200) for s in dispol_files]
 
-        dispol_array = np.empty((len(dispol), 200), dtype="c")
-        for i, d in enumerate(dispol):
-            dispol_array[i] = d
-
         self.info("Initializing GGchemistry")
-        fchem.fort_ggchem.init_taurex_chemistry(dispol_array, self._charge)
+
+        fchem.fort_ggchem.init_taurex_chemistry(dispol, self._charge)
 
         self.setup_abundances(abundance_profile)
         self.setup_ratios(ratio_elements, ratios_to_O)
+
         fchem.parameters.model_eqcond = equilibrium_condensation
 
         fchem.parameters.tfast = Tfast
@@ -201,33 +241,64 @@ class GGChemNonSafe(Chemistry):
         self.info("T-fast %s K", Tfast)
         self.info("Eq. Condensation: %s", equilibrium_condensation)
 
-        _mols = fchem.fort_ggchem.copy_molecule_names(fchem.chemistry.nmole)
-        mols = np.lib.stride_tricks.as_strided(_mols, strides=(_mols.shape[1], 1))
+        nmole = fchem.chemistry.nmole
 
-        mols = [s[0].decode("utf-8") for s in np.char.strip(mols.view("S20"))]
+        _mols = fchem.fort_ggchem.copy_molecule_names(nmole)
+        self.debug("Molecules %s", _mols)
 
+        mols = [s.decode("utf-8").strip() for s in _mols]
+        self.debug("Molecules decoded %s", mols)
         self._molecules = mols
-
-        self.restore_molecule_names()
 
         if dustchem_file is None:
             dustchem_file = self._base_data_path / "DustChem.dat"
 
         self.info("Loading dustchem file %s", dustchem_file)
         fchem.dust_data.dustchem_file = str(dustchem_file).ljust(200)
+
         fchem.init_dustchem_taurex()
+
+        ndust = fchem.dust_data.ndust
+
+        _dust = fchem.fort_ggchem.copy_dust_names(ndust)
+        dust = [s.decode("utf-8").strip() for s in _dust]
+        self.debug("Dust decoded %s", dust)
+        self._condensates = dust
 
         # quit()
         # self.update_abundances()
 
-        self._setup_active_inactive()
-        self.add_ratio_params()
+    @property
+    def condensates(self):
+        """
+        Returns a list of condensates in the atmosphere.
 
-    def setup_abundances(self, profile):
+        Returns
+        -------
+        active : :obj:`list`
+            List of condensates
+
+        """
+
+        return self._condensates
+
+    @property
+    def condensateMixProfile(self):
+        """
+        **Requires implementation**
+
+        Should return profiles of shape ``(ncondensates,nlayers)``.
+        """
+        if len(self.condensates) == 0:
+            return None
+        else:
+            return self._dust
+
+    def setup_abundances(self, profile: npt.NDArray[np.float64]) -> None:
 
         if profile is not None:
             self.info("Selected abundance profile %s", profile)
-            abundance_file = self._base_data_path, "Abundances.dat"
+            abundance_file = self._base_data_path / "Abundances.dat"
             chosen = 4  # 4 - earth 5 - ocean 6 - solar 7- meteorite
             profile = profile.lower()
 
@@ -267,9 +338,8 @@ class GGChemNonSafe(Chemistry):
                     elements.append(el)
                     ab = float(split_line[chosen])
                     self.info("%s %s", el, ab)
-                    abundances[self.get_element_index(el)] = ab
+                    self._initial_abundances[self.get_element_index(el)] = ab
 
-        # self.update_abundances(elements=elements,abundances=abundances)
         self._initial_abundances /= self._initial_abundances[0]
         self.info("H relative abundances.....")
         for el, ab in zip(self.allowed_elements, self._initial_abundances):
@@ -277,7 +347,10 @@ class GGChemNonSafe(Chemistry):
 
         # raise
 
-    def setup_ratios(self, ratio_elements, ratios_to_O):
+    def setup_ratios(
+        self, ratio_elements: t.List[str], ratios_to_O: npt.ArrayLike
+    ) -> None:
+        ratios_to_O = np.array(ratios_to_O)
         self._metal_idx = [
             self.get_element_index(e)
             for e in self._elements
@@ -315,16 +388,14 @@ class GGChemNonSafe(Chemistry):
         for met, rat in zip(self._metal_elements, self._ratios):
             self.info("%s %s", met, rat)
 
-    def get_element_index(self, element):
-        return (
-            fchem.chemistry.elnum[
-                int(getattr(fchem.chemistry, element.strip().lower())) - 1
-            ]
-            - 1
-        )
+    def get_element_index(self, element: str) -> int:
+        elnums = fchem.chemistry.elnum
+        index = getattr(fchem.chemistry, "el" + element.strip().lower(), None)
+
+        return elnums[index - 1] - 1
         # return self.get_element_index(element)
 
-    def _setup_active_inactive(self):
+    def _setup_active_inactive(self) -> None:
 
         active_mols = []
         inactive_mols = []
@@ -332,7 +403,9 @@ class GGChemNonSafe(Chemistry):
         inactive_indices = []
         nmol = fchem.chemistry.nmole
 
-        for idx, m in enumerate(self._elements + self._molecules):
+        clean_elements = [el if el != "el" else "e-" for el in self._elements]
+
+        for idx, m in enumerate(clean_elements + self._molecules):
             if m in self.availableActive:
                 active_mols.append(m)
                 active_indices.append(idx)
@@ -343,13 +416,14 @@ class GGChemNonSafe(Chemistry):
         self.info("Active molecules %s", active_mols)
         self.info("Inactive molecules %s", inactive_mols)
 
-        self._active_index = np.array(active_indices, dtype=np.integer)
-        self._inactive_index = np.array(inactive_indices, dtype=np.integer)
+        self._active_index = np.array(active_indices, dtype=np.int64)
+        self._inactive_index = np.array(inactive_indices, dtype=np.int64)
 
         self._active_mols = active_mols
         self._inactive_mols = inactive_mols
 
-    def restore_molecule_names(self):
+    def restore_molecule_names(self) -> None:
+        """Restores molecule names from GGChem style."""
         import re
 
         elm = [s for s in self._passed_elements if len(s) > 1]
@@ -365,8 +439,8 @@ class GGChemNonSafe(Chemistry):
             for s in self._molecules
         ]
 
-    def update_abundances(self):
-
+    def update_abundances(self) -> npt.NDArray[np.float64]:
+        """Updates the abundances of the system."""
         abundances = self._initial_abundances
         ratios = self._ratios
         O_abund = self._O_abund * self._metallicity
@@ -378,28 +452,27 @@ class GGChemNonSafe(Chemistry):
 
     def initialize_chemistry(
         self,
-        nlayers=100,
-        temperature_profile=None,
-        pressure_profile=None,
-        altitude_profile=None,
+        nlayers: t.Optional[int] = 100,
+        temperature_profile: t.Optional[npt.NDArray[np.float64]] = None,
+        pressure_profile: t.Optional[npt.NDArray[np.float64]] = None,
+        altitude_profile: t.Optional[npt.NDArray[np.float64]] = None,
     ):
         from taurex.constants import KBOLTZ
 
         ab = self.update_abundances()
-
-        # print(ab)
-
-        # fchem.structure.tgas[:nlayers] = temperature_profile
-        # fchem.structure.press[:nlayers] = pressure_profile*10 # to dyn/cm2
         self.info("Running GGChem equilibrium code...")
         nmol = fchem.chemistry.nmole
         nelem = fchem.chemistry.nelm
-        with stdout_redirector(self._stream_to_logger):
-            mols = [
-                fchem.fort_ggchem.run_ggchem(nelem + nmol, t, p * 10, ab)
-                for t, p in zip(temperature_profile, pressure_profile)
-            ]
+        ndust = fchem.dust_data.ndust
+        mols = []
+        dust = []
+        for t, p in zip(temperature_profile, pressure_profile):
+            m, d = fchem.fort_ggchem.run_ggchem(nelem + nmol, ndust, t, p * 10, ab)
+
+            mols.append(m)
+            dust.append(d)
         self._mols = np.stack(mols).T
+        self._dust = np.stack(dust).T
         # self._vmr = mols/np.sum(mols,axis=0)
         # self._vmr = self._mols
         # self._vmr = mols/1e-6 #m-3
@@ -461,7 +534,7 @@ class GGChemNonSafe(Chemistry):
     def inactiveGasMixProfile(self):
         return self._vmr[self._inactive_index]
 
-    def ratio(self, element_a, element_b):
+    def ratio(self, element_a: float, element_b: float) -> float:
         element_a = element_a.strip()
         element_b = element_b.strip()
 
@@ -474,5 +547,20 @@ class GGChemNonSafe(Chemistry):
     @classmethod
     def input_keywords(cls):
         return [
-            "ggchem-nonsafe",
+            "ggchem",
         ]
+
+    BIBTEX_ENTRIES = [
+        r"""
+        @article{ ggchem,
+            author = {{Woitke, P.} and {Helling, Ch.} and {Hunter, G. H.} and {Millard, J. D.} and {Turner, G. E.} and {Worters, M.} and {Blecic, J.} and {Stock, J. W.}},
+            title = {Equilibrium chemistry down to 100 K - Impact of silicates and phyllosilicates on the carbon to oxygen ratio},
+            DOI= "10.1051/0004-6361/201732193",
+            url= "https://doi.org/10.1051/0004-6361/201732193",
+            journal = {A\&A},
+            year = 2018,
+            volume = 614,
+            pages = "A1",
+        }
+        """
+    ]
