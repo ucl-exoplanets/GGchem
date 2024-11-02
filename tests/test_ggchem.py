af5cb1c7d14749a8b79ee1f4f30486173d3f955e
Ahmed <ahmed.al-refaie.12@ucl.ac.uk>
2024-11-02 09:24:22 +0000

    Updated test to not take ages

--- tests/test_ggchem.py
+++ tests/test_ggchem.py
@@ -27,11 +27,11 @@ def test_ggchem():
     import math
 
     gg = GGChem(equilibrium_condensation=True)
-    tp = np.logspace(math.log10(2500), math.log10(2000), 100)
+    tp = np.ones(1) * 2500
 
-    pp = np.ones(100) * 1e5
+    pp = np.ones(1) * 1e7
     pp
-    gg.initialize_chemistry(nlayers=100, temperature_profile=tp, pressure_profile=pp)
+    gg.initialize_chemistry(nlayers=1, temperature_profile=tp, pressure_profile=pp)
     assert np.all(gg.muProfile / AMU != 0.0)
 
 
@@ -44,9 +44,9 @@ def test_ggchem_unsafe():
     import math
 
     gg = GGChem(equilibrium_condensation=True)
-    tp = np.logspace(math.log10(2500), math.log10(2000), 100)
+    tp = np.ones(1) * 2500
 
-    pp = np.ones(100) * 1e5
+    pp = np.ones(1) * 1e7
     pp
-    gg.initialize_chemistry(nlayers=100, temperature_profile=tp, pressure_profile=pp)
+    gg.initialize_chemistry(nlayers=1, temperature_profile=tp, pressure_profile=pp)
     assert np.all(gg.muProfile / AMU != 0.0)
